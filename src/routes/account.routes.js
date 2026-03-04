a013755b038181726d1f405eacfdae88356fb873
Mariano Lopez <mariano@empowerme.global>
2026-03-04 13:32:48 -0600

    feat(account): add profile name update endpoint with joi validation

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express'
 import { AccountController } from '../controllers/account/account.controller.js'
+import { validate } from '../middlewares/validate.middleware.js'
+import { updateProfileSchema } from '../schemas/user.schema.js'
 
 const router = Router()
 
 router.get('/', AccountController.getAll)
+router.put('/profile', validate(updateProfileSchema), AccountController.updateProfile)
 router.get('/:companyId', AccountController.getCompanySetupByCompanyId)
 router.put('/:companyId', AccountController.update)
 
